0c033411ec2813d9d9b2f2c7c78ed7d1c941d2c6
dsaini64 <dsaini64@users.noreply.github.com>
2015-03-08 09:36:13 -0700

    added mergeSort

--- src/sortingAlgorithms.java
+++ src/sortingAlgorithms.java
@@ -63,6 +63,58 @@ public class sortingAlgorithms {
 		
 		return a;
 	}
+	
+	
+	public int[] mergeSort(int[] a){
+		
+		if(a.length < 2){
+			return a;
+		}
+		
+		int[] firstHalf = new int[a.length/2];
+		int[] secondHalf = new int[(a.length+1)/2];
+				
+		for(int i = 0; i < firstHalf.length;i++){
+			firstHalf[i] = a[i];	
+		}		
+		
+		for(int i = 0; i < secondHalf.length;i++){
+			secondHalf[i] = a[i + firstHalf.length];	
+		}
+		
+		mergeSort(firstHalf);
+		mergeSort(secondHalf);
+		
+		int ind1 = 0;
+		int ind2 = 0;
+		
+		for(int i = 0; ind1 < firstHalf.length && ind2 < secondHalf.length ; i++){
+			
+			if(firstHalf[ind1] <= secondHalf[ind2]){
+				a[i] = firstHalf[ind1];
+				ind1++;
+			}
+			else{
+				a[i] = secondHalf[ind2];
+				ind2++;
+			}
+		}
+		
+		if(ind1 != firstHalf.length){
+			for(int i = ind1; i < firstHalf.length;i++){
+				a[i + secondHalf.length] = firstHalf[i];
+			}
+		}else{
+			for(int i = ind2; i < secondHalf.length;i++){
+				a[i + firstHalf.length] = secondHalf[i];
+			}
+		}
+		
+		return a;
+		
+		
+		
+	}
 		
 	
 	public void printSorted(int[] a){
@@ -71,8 +123,5 @@ public class sortingAlgorithms {
 			System.out.println(a[i]);
 		}
 		
-		
-		
 	}
-
 }
